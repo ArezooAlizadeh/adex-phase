4af82d59d9b7b05eb4af30ac4a17389cc93429d1
Bahadir Kasap <bkasap@gmail.com>
2016-08-12 17:40:24 +0200

    added spike density function

--- plotphase.py
+++ plotphase.py
@@ -2,20 +2,34 @@ import numpy as np
 import pylab as pl
 from brian2 import *
 
-C = 480*pF              # Membrane Capacitance (pF)
-gL = 20*nS              # Leak Conductance (nS)
+def frate(st, window_width=5, dt=0.01):
+    '''
+    st : spike train
+    window_width : gaussian kernel width in ms
+    dt : time step in ms
+    '''
+    width_dt = int(window_width/dt)
+    window = exp(-arange(-4 * width_dt, 4 * width_dt + 1) ** 2 * 1. / (2 * (width_dt) ** 2))
+    rate = zeros(int(500./(dt)))
+    if len(st)>0:
+        st = [int(x/dt) for x in st]
+        rate[st] = 1000./dt
+    return convolve(rate, window * 1. / sum(window), mode='same')
+
+C = 250*pF              # Membrane Capacitance (pF)
+gL = 15*nS              # Leak Conductance (nS)
 DeltaT = 2*mV           # Spike Slope Factor (mV)
 taum = C/gL             # Membrane Time Constant (pF/nS=ms)
-EL = -53*mV             # Leak Reversal Potential (mV)
+EL = -55*mV             # Leak Reversal Potential (mV)
 VT = -50*mV             # Spike Threshold (mV)
 Vr = -45*mV             # Reset potential (mV)
 Vr2 = -20*mV
 Vcut = 0*mV             # Spiking Threshold (mV)
 a = 4*nS                # Subthreshold Adaptation (nS)
-b = 120*pA              # Spike-triggered Adaptation (nA)
-tauw = 80*ms            # Adaptation Time Constant (ms)
+b = 60*pA              # Spike-triggered Adaptation (nA)
+tauw = 120*ms            # Adaptation Time Constant (ms)
 
-inputIcl = 600*pA
+inputIcl = 700*pA
 vms = np.linspace(-120*mV, Vcut, 2000)
 
 vnull =gL*(EL-vms)+gL*DeltaT*exp((vms-VT)/DeltaT)+inputIcl #v-nullcline
@@ -78,14 +92,14 @@ def ppp():
     Neurontra.w[0] = 0*amp
 
     print "stimulation start"
-    run(100*ms, report='text', report_period=1*second)
+    run(50*ms, report='text', report_period=1*second)
     print "stimulation over"
 
     inputI = 0*nA
     Neuron.I=inputI
     Neurontra.I=inputI
 
-    run(100*ms, report='text', report_period=1*second)
+    run(150*ms, report='text', report_period=1*second)
 
 
     pl.figure()
@@ -105,20 +119,28 @@ def ppp():
     pl.ylim(-0.5, 1.5)
     pl.xlabel("Membrane Potential (mV)")
     pl.ylabel("Adaptive current (nA)")
-
-    pl.figure()
-    pl.subplot(211)
-    pl.plot(Mtra.t/ms, Mtra.vm[0]/mV)
-    pl.xlim(100, 300)
     
+    
+    pl.figure()
+    xstart = 80
+    xend = 200
+    pl.subplot(311)
+    pl.plot(Mtra.t/ms, Mtra.vm[0]/mV)
+    pl.xlim(xstart, xend)
     pl.ylabel("Membrane potential (mV)")
     
-    pl.subplot(212)
+    pl.subplot(312)
     pl.plot(Mtra.t/ms, Mtra.w[0]/nA)
-    pl.xlim(100, 300)
-    
-    pl.xlabel("Time (ms)")
+    pl.xlim(xstart, xend)
     pl.ylabel("Adaptive Current (nA)")
+
+    pl.subplot(313)
+    strain = stra.spike_trains()
+    firing = frate(strain[0]/ms)
+    pl.plot(np.linspace(0, 500, len(firing)), firing)
+    pl.xlim(xstart, xend)
+    pl.xlabel("Time (ms)")
+    pl.ylabel("Spike density (spks/s)")
     
     pl.show()
     print stra.num_spikes
